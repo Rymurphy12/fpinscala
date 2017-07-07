e71cce098ea48a23e7cb11da1878a0643d877344
Ryan Murphy <rymurphy12@gmail.com>
2017-07-07 16:14:51 -0500

    "Completed up to Exercise 6.5"

--- src/main/scala/state/Random.scala
+++ src/main/scala/state/Random.scala
@@ -1,8 +1,16 @@
 package fpinscala.state
 
 trait RNG {
+
+  type Rand[+A] = RNG => (A, RNG)
+
   def nextInt: (Int, RNG)
 
+  def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng)
+    }
   //Exercise 6.1
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
     val (i, rng2) = rng.nextInt
@@ -14,9 +22,49 @@ trait RNG {
   //Exercise 6.2
   def double(rng: RNG): (Double, RNG) = {
     val (i, rng2) = rng.nonNegativeInt(rng)
-    (i / Int.MaxValue.toDouble, rng2)
+    (i / (Int.MaxValue.toDouble + 1), rng2)
   }
+
+  //Exercise 6.3
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, rng1) = rng.nextInt
+    val (d, rng2) = double(rng1)
+    ((i,d), rng2)
+  }
+
+  //Exercise 6.3
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val ((i,d), rng1) = intDouble(rng)
+    ((d, i), rng1)
+  }
+
+  //Exercise 6.3
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, rng1) = double(rng)
+    val (d2, rng2) = double(rng1)
+    val (d3, rng3) = double(rng2)
+    ((d1,d2,d3), rng3)
+  }
+
+  //Exercise 6.4
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    def go(count: Int, xs: List[Int])(rng: RNG): (List[Int], RNG) = {
+      if (count == 0)
+        (xs,rng)
+      else{
+        val (i, r) = rng.nextInt
+        go(count - 1, i :: xs)(r)
+      }
+    }
+    go(count, Nil)(rng)
+  }
+
+  //Exercise 6.5
+  def doubleViaMap(rng: RNG): Rand[Double] =
+    map(nonNegativeInt)(_ / (Int.MaxValue.toDouble + 1))
 }
+
+
 
 case class SimpleRNG(seed: Long) extends RNG {
   def nextInt: (Int, RNG) = {
