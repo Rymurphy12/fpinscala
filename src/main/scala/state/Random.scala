a00299731df1fa325bacbd39e295a90cbb8658dc
Ryan Murphy <rymurphy12@gmail.com>
2017-07-10 14:55:20 -0500

    "Completed up to Exercise 6.7"

--- src/main/scala/state/Random.scala
+++ src/main/scala/state/Random.scala
@@ -6,11 +6,6 @@ trait RNG {
 
   def nextInt: (Int, RNG)
 
-  def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
-    rng => {
-      val (a, rng2) = s(rng)
-      (f(a), rng)
-    }
   //Exercise 6.1
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
     val (i, rng2) = rng.nextInt
@@ -59,9 +54,33 @@ trait RNG {
     go(count, Nil)(rng)
   }
 
+  def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng)
+    }
+
   //Exercise 6.5
   def doubleViaMap(rng: RNG): Rand[Double] =
     map(nonNegativeInt)(_ / (Int.MaxValue.toDouble + 1))
+
+  //Exercise 6.6
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A,B) => C): Rand[C] =
+    rng => {
+      val (a, rng1) = ra(rng)
+      val (b, rng2) = rb(rng1)
+      (f(a,b), rng2)
+    }
+
+  def unit[A](a: A): Rand[A] =
+    rng => (a, rng)
+
+  def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] =
+    map2(ra, rb)((_,_))
+
+  //Exercise 6.7
+  def sequence[A](fs: List[Rand[A]]):Rand[List[A]] =
+    fs.foldRight(unit(List[A]()))((x,y) => map2(x, y)(_ :: _))
 }
 
 
