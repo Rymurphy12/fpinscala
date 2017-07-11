ea6deedec067fdace7d7433e16791baeae51f3bc
Ryan Murphy <rymurphy12@gmail.com>
2017-07-11 16:26:40 -0500

    "Completed Exercise 6.8"

--- src/main/scala/state/Random.scala
+++ src/main/scala/state/Random.scala
@@ -81,6 +81,21 @@ trait RNG {
   //Exercise 6.7
   def sequence[A](fs: List[Rand[A]]):Rand[List[A]] =
     fs.foldRight(unit(List[A]()))((x,y) => map2(x, y)(_ :: _))
+
+  //Exercise 6.8
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+    rng => {
+      val (a, rng1) = f(rng)
+      g(a)(rng1)
+    }
+
+  //Exercise 6.8
+  def nonNegativeLessThan(n: Int): Rand[Int] = {
+    flatMap(nonNegativeInt) { i =>
+      val mod = i % n
+      if (i + (n-1) - mod >= 0) unit(mod) else nonNegativeLessThan(n)
+    }
+  }
 }
 
 
