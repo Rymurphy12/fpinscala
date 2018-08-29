cb33ee382e5c615157841835540fd33b52aa79f9
rymurphy12 <strife12>
2018-08-29 14:40:16 -0500

    Finished Chapter 2 Exercises

--- src/main/scala/gettingstarted/Exercises.scala
+++ src/main/scala/gettingstarted/Exercises.scala
@@ -5,6 +5,7 @@ object Exercises {
 
   // Exercise 2.1
   def fib(n: Int): Int = {
+    @annotation.tailrec
     def go(fst: Int, snd: Int, n: Int): Int =
       if (n == 0)
         fst
@@ -15,6 +16,7 @@ object Exercises {
 
   // Exercise 2.2
   def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    @annotation.tailrec
     def loop(n: Int): Boolean =
       if (n >= as.length -1)
         true
@@ -24,4 +26,16 @@ object Exercises {
         false
     loop(0)
   }
+
+  //Exercise 2.3
+  def curry[A,B,C](f: (A,B) => C): A => (B => C) =
+    a => b => f(a,b)
+
+  //Exercise 2.4
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C =
+    (a,b) => f(a)(b)
+
+  //Exercise 2.5
+  def compose[A,B,C](f: B => C, g: A => B): A => C =
+    a => f(g(a))
 }
